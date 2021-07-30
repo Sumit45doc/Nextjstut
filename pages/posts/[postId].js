b6ebf3cc1debf2879475a9d6de61415941d06162
sumit45doc <ss8559283@gmail.com>
2021-07-30 23:29:49 +0530

    fallback:true

--- pages/posts/[postId].js
+++ pages/posts/[postId].js
@@ -1,5 +1,10 @@
+import { useRouter } from "next/router";
 
 function Post({ post }) {
+    const router = useRouter();
+    if (router.isFallback) {
+        return <h1>Loading ...</h1>
+    }
     return (
         <div>
             <h2>{post.id} {post.title}</h2>
@@ -10,13 +15,20 @@ function Post({ post }) {
 export default Post
 
 export async function getStaticPaths() {
+    // const response = await fetch("https://jsonplaceholder.typicode.com/posts");
+    // const data = await response.json();
+    // const paths = data.map((post) => {
+    //     return {
+    //         params: {postId: `${post.id}`}
+    //     }
+    // });
     return {
         paths: [
-            {params: {postId : "1"}},
-            {params: {postId : "2"}},
-            {params: {postId : "3"}},
+            { params: { postId: "1"}},
+            { params: { postId: "2"}},
+            { params: { postId: "3"}},
         ],
-        fallback: false
+        fallback: true
     }
 }
 
@@ -26,6 +38,12 @@ export async function getStaticProps(context) {
     const { params } = context;
     const response = await fetch(`https://jsonplaceholder.typicode.com/posts/${params.postId}`)
     const data = await response.json();
+
+    if (!data.id) {
+        return {
+            notFound: true
+        }
+    }
 
     return {
         props: {
