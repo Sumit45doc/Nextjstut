20c1d78b2d0c5a55101f06fadeafcc69c42a8f5f
sumit45doc <ss8559283@gmail.com>
2021-08-02 21:55:17 +0530

    api DELETE

--- pages/api/comments/index.js
+++ pages/api/comments/index.js
@@ -1,4 +1,14 @@
 import { comments } from "../../../data/comments";
 export default function handler(req, res) {
-    res.status(200).json(comments)
+    if (req.method === "GET") {
+        res.status(200).json(comments)
+    } else if (req.method === "POST") {
+        const len = comments.length
+        const comment = {
+            id: len + 1,
+            text: req.body.comment
+        }
+        comments.push(comment);
+        res.status(201).json(comments)
+    }
 }
