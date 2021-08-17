993cc48544d94cfb5dd130db7dd68e797b43ba23
sumit45doc <ss8559283@gmail.com>
2021-08-17 15:59:32 +0530

    environment variable

--- pages/posts/index.js
+++ pages/posts/index.js
@@ -3,6 +3,9 @@ import Link from "next/link";
 function PostList({ posts }) {
     return (
         <div>
+           USERNAME: {process.env.DB_USER}
+            DB_PASSWORD: {process.env.DB_PASSWORD}
+            ID: {process.env.NEXT_PUBLIC_ID}
             {posts.map((post) => {
                 return (
                     <div key={post.id}>
@@ -19,7 +22,9 @@ function PostList({ posts }) {
 export async function getStaticProps() {
     const response = await fetch("https://jsonplaceholder.typicode.com/posts");
     const data = await response.json();
-
+    const username = process.env.DB_USER
+    const password = process.env.DB_PASSWORD
+    console.log(`username: ${username} & password: ${password}`)
     return {
         props: {
             posts: data.slice(0,3)
